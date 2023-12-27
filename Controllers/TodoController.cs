d63422e5205a45140ff4ac00b2faaf4478b8bf62
SardarBadarSaghir <dataentrybadar@gmail.com>
2023-12-27 11:15:53 +0500

    added fetching

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -5,17 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using mvc.Data;
 
-namespace mvc.Controllers
+namespace mvc.Controllers;
+
+// [Route("[controller]")]
+public class TodoController : Controller
 {
-    [Route("[controller]")]
-    public class TodoController : Controller
-    {
+    private readonly DbApplicationContext _db;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public TodoController(DbApplicationContext db)
+    {
+        _db = db;
 
     }
+    public IActionResult Index()
+    {
+      var objTodoList =  _db.Todos.ToList();
+
+        return View();
+    }
+
 }
